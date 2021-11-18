4a427d15cb65a3e430edf144cd327099949143b0
Kirk Shoop <kirk.shoop@gmail.com>
2021-11-17 19:55:42 -0800

    combine_each example fixes

--- examples/combine_each.cpp
+++ examples/combine_each.cpp
@@ -30,6 +30,15 @@ using namespace unifex;
 
 using namespace std::literals::chrono_literals;
 
+struct counter {
+  int inc() const { return ++count(); }
+  int load() const { return count().load(); }
+  static std::atomic<int>& count() {
+    static std::atomic<int> count_{0};
+    return count_;
+  }
+};
+
 const auto gap = std::array{3ms, 5ms};
 const auto initial = 200ms;
 const auto sample = 100;
@@ -52,26 +61,36 @@ int main() {
     auto tickCount = unifex::sync_wait(
         // send intervals (first, first + gap, etc..)
         unifex::combine_each(
-            unifex::interval(first, gap[0]),
-            unifex::interval(first, gap[1]) |
+            unifex::interval(first, gap[0]) |  //
+                then_each(
+                    [](auto expected) { return std::make_tuple(0, expected); }),
+            unifex::interval(first, gap[1]) |  //
+                then_each([](auto expected) {
+                  return std::make_tuple(1, expected);
+                }) |
                 // use a different thread for this interval
                 with_query_value(
                     unifex::get_scheduler, time[1].get_scheduler())) |  //
         // only sample 1 tick out of the expected, ignore the rest
-        filter_each(
-            [&](auto tick) { return (tick - first) % expected < gap[0]; }) |  //
+        filter_each([&](const auto& v) {
+          const auto& [id, tick] = v;
+          return (tick - first) % expected < gap[0];
+        }) |
         // include actual time of event
-        then_each([](auto tick) {
-          return std::make_tuple(std::chrono::steady_clock::now(), tick);
-        }) |  //
+        then_each([](auto v) {
+          auto& [id, tick] = v;
+          return std::make_tuple(id, std::chrono::steady_clock::now(), tick);
+        }) |
         // log ticks that arrive and accumulate a count of the ticks that
         // arrived
         reduce_each(
-            0,
-            [&](int count, const auto& itemSender) {
+            counter{},
+            [&](counter count, const auto& itemSender) {
+              int thisCount = count.inc();
               return itemSender |  //
-                  unifex::then([&, count]([[maybe_unused]] auto tpl) {
-                       auto& [actual, intended] = tpl;
+                  unifex::then([&, count, thisCount](
+                                   [[maybe_unused]] auto tpl) {
+                       auto& [id, actual, intended] = tpl;
                        auto delta =
                            std::chrono::duration_cast<
                                std::chrono::duration<float, std::milli>>(
@@ -83,17 +102,18 @@ int main() {
                                actual - first)
                                .count();
                        printf(
-                           "delta is %.4fms at sample %3d, %3.4fms after "
+                           "[%d] delta is %.4fms at sample %3d, %3.4fms after "
                            "initial tick\n",
+                           id,
                            delta,
-                           count,
+                           thisCount,
                            millis);
-                       return count + 1;
+                       return count;
                      });
             }) |  //
         // cancel the infinite interval sequence after 6 samples
         unifex::stop_when(
-            unifex::schedule_at(first + (expected * 6)) |  //
+            unifex::schedule_at(first + (expected * 5) + gap[0]) |  //
             then([] { printf("stop\n"); })) |
         // use this scheduler implicitly (first interval and the stop_when
         // trigger)
@@ -102,6 +122,6 @@ int main() {
         std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(
             std::chrono::steady_clock::now() - start)
             .count();
-    printf("emitted %d samples in %3.4fms\n", *tickCount, millis);
+    printf("emitted %d samples in %3.4fms\n", tickCount->load(), millis);
   }
 }
